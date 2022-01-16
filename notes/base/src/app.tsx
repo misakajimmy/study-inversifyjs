0616e82e07788dd54f203dd3837878558cd5b67d
Misaka Jimmy <misakajimmy@gmail.com>
2022-01-17 02:23:39 +0800

    first version of base note

---
+++ notes/base/src/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Home} from './home';
+
+export function App() {
+    return <Router>
+        <Switch>
+            <Route path='/' exact component={Home}/>
+        </Switch>
+    </Router>
+}
---
+++ notes/base/src/home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {injectable} from 'inversify';
+
+@injectable()
+export class Home extends React.Component<any, any> {
+
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return <div>
+
+        </div>
+    }
+
+}
